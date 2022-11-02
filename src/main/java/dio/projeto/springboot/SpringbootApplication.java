06876b5f1fe9bd98de1f443ad569aff66eb7d5ba
unknown <ricardobrunob@gmail.com>
2022-11-02 20:28:41 -0300

    projeto criado

---
+++ src/main/java/dio/projeto/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package dio.projeto.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dio/projeto/springboot/model/Cnpj.java
@@ -0,0 +1,186 @@
+package dio.projeto.springboot.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Generated("jsonschema2pojo")
+@Entity
+public class Cnpj {
+
+    @Id
+    private String nomeFantasia;
+    private String razaoSocial;
+    private String cnpj;
+    private String status;
+    private String cnaePrincipalDescricao;
+    private String cnaePrincipalCodigo;
+    private String cep;
+    private String dataAbertura;
+    private String ddd;
+    private String telefone;
+    private String email;
+    private String tipoLogradouro;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String municipio;
+    private String uf;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getNomeFantasia() {
+        return nomeFantasia;
+    }
+
+    public void setNomeFantasia(String nomeFantasia) {
+        this.nomeFantasia = nomeFantasia;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCnaePrincipalDescricao() {
+        return cnaePrincipalDescricao;
+    }
+
+    public void setCnaePrincipalDescricao(String cnaePrincipalDescricao) {
+        this.cnaePrincipalDescricao = cnaePrincipalDescricao;
+    }
+
+    public String getCnaePrincipalCodigo() {
+        return cnaePrincipalCodigo;
+    }
+
+    public void setCnaePrincipalCodigo(String cnaePrincipalCodigo) {
+        this.cnaePrincipalCodigo = cnaePrincipalCodigo;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(String dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipoLogradouro() {
+        return tipoLogradouro;
+    }
+
+    public void setTipoLogradouro(String tipoLogradouro) {
+        this.tipoLogradouro = tipoLogradouro;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
